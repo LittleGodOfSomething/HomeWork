3d00d6972d7666cf1ebed369b774c1940592a87f
Alex Nevon <lexaiiimejibnevon@gmail.com>
2018-10-15 14:21:37 +0300

    Empty

---
+++ HW3/Abonent/Abon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abonent
+{
+    class Abon
+    {
+        //поля
+        private string id;
+        private string fio;
+        private string adress;
+        private string cardNum;
+        private int debet;
+        private int credit;
+        private int talkTime;
+        private int crossTownTime;
+        
+
+        private bool PassCheck(string ePass)
+        {
+            if (ePass == "*8werb34-+bdd/`3vs")//сюда можно втавить пароль из пассГенератора какого-нибудь
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public Abon(string fio, string id, string adress, string cardNum, int debet, int credit, int talkTime, int crossTownTime, string AdminPass)//конструктор заполнения
+        {
+            if (PassCheck(AdminPass))
+            {
+                this.fio = fio;
+                this.id = id;
+                this.adress = adress;
+                this.cardNum = cardNum;
+                this.debet = debet;
+                this.credit = credit;
+                this.talkTime = talkTime;
+                this.crossTownTime = crossTownTime;
+            }
+            else
+            {
+                ///Destroy(this);
+                Console.WriteLine("Неверный пароль Администратора, бандит. :)");
+                return;
+            }
+            
+        }
+               
+        public string FIO {get { return fio; } }//гет ФИО
+        public int GetTalkTime { get { return talkTime; } }//гет Время балака
+        public int GetCrossTownTalkTime { get { return crossTownTime; } }//гет время междуТаун балака
+
+        public void Info()//вывод информации об этом абоненте
+        {
+            Console.Write($"\nФИО: {fio}\nИНН: {id}\nАдрес: {adress}\nНомер карты: {cardNum}\nДебет: {debet}\nКредит: {credit}\nВремя разговоров по городу: {AdoptedTime(talkTime)}\nВремя междугородних: {AdoptedTime(crossTownTime)}\n");
+        }
+
+        private string AdoptedTime(int time)//конвертируем время из секунд в нормальное для удобства всприятия
+        {
+            int c = time % 60;
+            int a = (int)((time / 60) / 60);
+            int b = (int)((time / 60) % 60);
+
+            return String.Format("{0}:{1}:{2}", a.ToString("00"), b.ToString("00"), c.ToString("00"));
+        }
+    }
+}
---
+++ HW3/Abonent/PersonalityGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Abonent
+{
+    class PersonalityGenerator
+    {
+        private string names;
+        private string cities;
+        private string streets;
+        private string[] sortedNames;
+        private string[] sortedCities;
+        private string[] sortedStreets;
+        private Random random;
+
+        public PersonalityGenerator()
+        {
+            random = new Random();
+            names = File.ReadAllText(@"source\names.dll");
+            cities = File.ReadAllText(@"source\cities.dll");
+            streets = File.ReadAllText(@"source\streetsSorted.dll");
+            sortedNames = names.Split('+', StringSplitOptions.RemoveEmptyEntries);
+            sortedCities = cities.Split('+', StringSplitOptions.RemoveEmptyEntries);
+            sortedStreets = streets.Split('+', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public string GetName()
+        {
+            return sortedNames[random.Next(0, (sortedNames.Length - 1))];
+        }
+
+        public string GetCity()
+        {
+            return sortedCities[random.Next(0, (sortedCities.Length - 1))];
+        }
+
+        public string GetStreet()
+        {
+            return sortedStreets[random.Next(0, (sortedStreets.Length - 1))];
+        }
+
+        public string GetHomeNumber()
+        {
+            return (random.Next(0, 200)).ToString();
+        }
+
+        public string GetAppartmentNumber()
+        {
+            return (random.Next(0, 1000)).ToString();
+        }
+
+        public string GetCardNumber()
+        {
+            return String.Format("{0}-{1}-{2}-{3}", random.Next(4012, 4301).ToString("0000"), random.Next(0, 9999).ToString("0000"), random.Next(0, 9999).ToString("0000"), random.Next(0, 9999).ToString("0000"));
+        }
+
+        public int GetDebit()
+        {
+            return random.Next(0, 5000);
+        }
+        public int GetCredit()
+        {
+            return random.Next(0, 5000);
+        }
+
+        public int GetTimeSec()
+        {
+            return random.Next(0, 36001);
+        }
+
+        public string GetINN()
+        {
+            return String.Format("{0}{1}{2}", random.Next(1000, 10000).ToString("0000"), random.Next(0, 10000).ToString("0000"), random.Next(0, 10000).ToString("0000"));
+        }
+
+
+    }
+}
